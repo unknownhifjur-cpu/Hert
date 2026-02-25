98bbe52b2f6f10d12ed2622ba6a878c3fe5a6311
Hifjur Alam <unknownhifjur@gmail.com>
2026-02-25 13:02:33 +0530

    fine

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: 'https://unknownhifjur-hert-backend.vercel.app/api',
 });
 
 api.interceptors.request.use(
